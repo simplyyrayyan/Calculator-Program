cd5b4d3b4954861a3081eede8ca2773e1403c836
simplyyrayyan <simplyyrayyan@gmail.com>
2026-06-28 15:21:25 -0400

    Refactor calculator to use slow_print and slow_input
    Refactor calculator program to use slow_print and slow_input for user interactions. Improved operator validation and error handling.

--- calculator.py
+++ calculator.py
@@ -1,85 +1,84 @@
 # Calculator Program 
 
 # Imports
-import time, sys
+import sys
+from slow_printing import slow_print, slow_input
 
+operator = ['+', '-', '*', '/', '**']
 # Functions
 
 # Ask To Contiue Function
 def ask_to_continue():
-    user_input1 = input("Would you like to (Quit) or (Continue)?: ").lower().strip()
-    if user_input1 == "quit":
-        time.sleep(0.8)
-        print("Thanks for using the Calculator Program hope to see you again!")
+    user_countiue = slow_input("Would you like to (Quit) or (Continue)?: ").lower().strip()
+    if user_countiue == "quit":
+        slow_print("Thanks for using the Calculator Program hope to see you again!")
         sys.exit()
-    elif user_input1 == "continue":
-        time.sleep(0.8)
+    elif user_countiue == "continue":
         return True
     else:
-        print("Invalid Number")
+        slow_print("Invalid Number")
         return True
 
 # Greeting
-print("Welcome to the Calculator Program!")
+slow_print("Welcome to the Calculator Program!")
 
 # Main Loop
-
-while True: 
-    # Collecting User Input and Validating the Operator
-    operator = input("Enter the Operator you would like to use + - * / **: ")
-    if operator not in ['+', '-', '*', '/', '**']:
-        print("Invalid operator.")
-        continue
-
-
-# Collecting User Input and Validating the Numbers
-    try:
-        num1 = float(input("Enter the first number: "))
-
-        num2 = float(input("Enter the second number: "))
-        time.sleep(0.3)
-    except TypeError as e1:
-        time.sleep(1)
-        print(f"You have error {e1}, Please Try Again")
-        continue
-    except ValueError as e2:
-        time.sleep(1)
-        print(f"You have error {e2}, Please Try Again")
-        continue
-
-# Calculating the Result
-
-    # Addition
-    if operator == "+":
-        result = num1 + num2
-    
-    # Subtracting
-    elif operator == "-":
-        result = num1 - num2
-    
-    # Multiplying
-    elif operator == "*":
-        result = num1 * num2
-    
-    # Division
-    elif operator == "/":
-        if num2 == 0:
-            print("Error: Division by zero is not allowed.")
+def main():
+    while True: 
+        # Collecting User Input and Validating the Operator
+        operators = slow_input("Enter the Operator you would like to use (*operator): ")
+        if operators not in operator:
+            slow_print("Invalid operator.")
             continue
-        else:
-            result = num1 / num2
 
-    # Exponetial
-    elif operator == "**":
-        result = num1 ** num2
+
+    # Collecting User Input and Validating the Numbers
+        try:
+            num1 = float(slow_input("Enter the first number: "))
+            num2 = float(slow_input("Enter the second number: "))
+        except TypeError:
+            slow_print(f"You have a TypeError, Please Try Again")
+            continue
+        except ValueError:
+            slow_print(f"You have a ValueError, Please Try Again")
+            continue
+
+    # Calculating the Result
+
+        # Addition
+        if operators == "+":
+            result = num1 + num2
+
+        # Subtracting
+        elif operators == "-":
+            result = num1 - num2
+
+        # Multiplying
+        elif operators == "*":
+            result = num1 * num2
     
-    # Invalid Operator
-    elif operator not in ['+', '-', '*', '/', '**']:
-        print("Invalid operator.")
-        print("Please Try Again")
-        continue
+        # Division
+        elif operators == "/":
+            if num2 == 0:
+                slow_print("Error: Division by zero is not allowed.")
+                continue
+            else:
+                result = num1 / num2
+
+        # Exponetial
+        elif operators == "**":
+            result = num1 ** num2
+    
+        # Invalid Operator
+        elif operators not in ['+', '-', '*', '/', '**']:
+            slow_print("Invalid operator.")
+            slow_print("Please Try Again")
+            continue
 
 
-    print(f"Your answer is: {result}")
-    ask_to_continue()
-# Code Ends
+        print(f"Your answer is: {result}")
+        ask_to_continue()
+    # Code Ends
+
+if __name__ == "__main__":
+    main()
